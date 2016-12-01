fce0a89cd38831c9df6409824f01f11a385c989e
antoniop-lm <antoniop.lm@hotmail.com>
2016-11-30 22:37:34 -0200

    Esboco parte 5

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 import pickle
 
+N_INPUT = 307200
 N_HIDDEN = 45
 N_OUTPUT = 15
 ITERATIONS = 6000
@@ -20,7 +21,7 @@ class Classifica:
         self.predict(inputs, labels);
         for i in xrange(labels.rows):
             for j in xrange(labels.cols):
-                labels[j][i] = labels[j][i] + 0.5
+                labels[j][i] = float(int(labels[j][i] + 0.5))
                 
 
 # Classe com as imagens abertas
@@ -57,7 +58,7 @@ class Images:
     
 def Treina_MLP(self,trainingDataMat,labelsMat):
     net = np.zeros((1, 3), dtype = "CV_32SC1")
-    net[0][0] = 15          # entrada
+    net[0][0] = N_INPUT     # entrada
     net[0][1] = N_HIDDEN    # camadaOculta
     net[0][2] = N_OUTPUT    # saida
 
@@ -84,32 +85,36 @@ def Testa_MLP(self,trainingDataMat,labelsMat):
     for i = 0 in xrange(trainingDataMat.rows):
         print("PM = " + str(trainingDataMat[i][0]) + " MI = " + str(trainingDataMat[i][1]) + " Label: " + str(labelsMat[i]) + " Predict: " + str(resp[i]) + " Erro: " + str(labelsMat[i] - resp[i]))
         
-        predict = (resp[i] + 0.5)
+        predict = int(resp[i] + 0.5)
         total = total + 1
-        if(predict != labelsMat[i]):  
+        if(predict != int(labelsMat[i])):  
             errados = errados + 1
     
     print("Total: " + str(total) + " Acuracia: " + str(100.0 * ((total - errados) / total)) + "%")
     
-    
+def CompareResult(self,actualResult,lastResult):
+    print("em andamento")
+
 flag = raw_input("Escolha um opcao:\n[1] Treina a Rede\n[2] Classifica uma imagem\n[3]Analisa sequencia de movimentos")
 path_dir = "/home/antonio/Documentos/SensoresInteligentes/Base/test/"
-image = []
 
 if (int(flag) == 1):
+    trainingDataMat = np.zeros((15, N_INPUT))
+    labelsMat = np.zeros((15,15))
     # abre a base de dados
     for k in xrange(15):
         image_file = str(k) + ".jpg"
         fp = Images(misc.imread(path_dir + image_file, flatten=1),k)
         for i in xrange(fp.getImageHeight()):
             for j in xrange(fp.getImageWidth()):
-                fp.setImageIndex(i,j,fp.getImageIndex(i,j)/255.0)
+                trainingDataMat[k][i*fp.getImageWidth()+j] = fp.getImageIndex(i,j)/255.0
         
-        image.append(fp)
+        labelsMat[k][k] = 1
         
-    Treina_MLP()
-
+    Treina_MLP(trainingDataMat,labelsMat)
 elif(int(flag) == 2):
+    trainingDataMat = np.zeros((1, N_INPUT))
+    labelsMat = np.zeros((1,15))
     # abre a imagem que vai ser classificada e seleciona o que ela representa
     image_file = raw_input("Digite o nome da imagem: ")
     print("[Indice da Saida]\n(0 1 2 3 4)\n(5 6 7 8 9)\n(10 11 12 13 14)")
@@ -117,21 +122,32 @@ elif(int(flag) == 2):
     fp = Images(misc.imread(path_dir + image_file, flatten=1),int(index))
     for i in xrange(fp.getImageHeight()):
         for j in xrange(fp.getImageWidth()):
-            fp.setImageIndex(i,j,fp.getImageIndex(i,j)/255.0)
+            trainingDataMat[0][i*fp.getImageWidth()+j] = fp.getImageIndex(i,j)/255.0
                 
-    image.append(fp)
+    labelsMat[0][int(index)] = 1
     
-    Testa_MLP()
+    Testa_MLP(trainingDataMat,labelsMat)
 elif(int(flag) == 3):
+    trainingDataMat = np.zeros((1, N_INPUT))
+    labelsMat = np.zeros((1,15))
+    lastResult = np.zeros((1,15))
+    actualResult = np.zeros((1,15))
     cap = cv2.VideoCapture(0)
 
     while(1):
-        ret ,frame = cap.read()
+        ret,frame = cap.read()
 
         if ret == True:
             cv2.imshow('img2',frame)
             
-            Testa_MLP()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            for i in xrange(cap.get(4)):
+                for j in xrange(cap.get(3)):
+                    trainingDataMat[0][i*cap.get(3)+j] = frame[i][j]/255.0
+            
+            actualResult = Testa_MLP(trainingDataMat,labelsMat)
+            CompareResult(actualResult,lastResult)
+            lastResult = actualResult
 
             k = cv2.waitKey(60) & 0xff
             if k == 27:
