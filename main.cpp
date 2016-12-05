f59fc041f45d1b1339707b3066c2c48da567878e
antoniop-lm <antoniop.lm@hotmail.com>
2016-12-04 23:23:12 -0200

    Programa totalmente codado, pronto para testes

--- main.cpp
+++ main.cpp
@@ -5,49 +5,132 @@
 using namespace std;
 using namespace cv;
 
-#define N_INPUT 307200
-#define N_HIDDEN 45
-#define N_OUTPUT 15
-#define ITERATIONS 6000
-#define ERROR 0.01
+#define N_INPUT 307200  // numero de neuronios na input layer
+#define N_HIDDEN 45     // numero de neuronios na hidden layer
+#define N_OUTPUT 15     // numero de neuronios na output layer
+#define ITERATIONS 6000 // numero maximo de iteracoes
+#define ERROR 0.01      // erro minimo - threshold
 
-String path_dir = "/home/antonio/Documentos/SensoresInteligentes/Base/test/"
+String path_dir = "/home/antonio/Documentos/SensoresInteligentes/Base/test/";
 
+/* Funcao que treina a Multilayer Perceptron
+ * Parametros:
+ *              trainingDataMat - base de dados do treinamento
+ *              labelsMat       - saida esperada de cada dado de treinamento
+ */
 void Treina_MLP(Mat trainingDataMat, Mat labelsMat) { 
-    Mat net = Mat(1,3,CV_32SC1);
+    Mat net = Mat::zeros(1,3,CV_32SC1); // cria os dados de cada camada de neuronios
     net.at<int>(0,0) = N_INPUT;
     net.at<int>(0,1) = N_HIDDEN;
     net.at<int>(0,2) = N_OUTPUT;
+    // carrega os parametros para o backpropagation
     CvANN_MLP_TrainParams params = CvANN_MLP_TrainParams(cvTermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, ITERATIONS, ERROR),
                                                          CvANN_MLP_TrainParams::BACKPROP, 0.1, 0.1);
     
+    // cria a MLP com os dados da funcao de ativacao e os parametros definidos anteriormente, salvando apos o treino
     CvANN_MLP *mlp = new CvANN_MLP();
     mlp->create(net, CvANN_MLP::SIGMOID_SYM, 0.6, 1);
     mlp->train(trainingDataMat, labelsMat, Mat(), Mat(), params);
     mlp->save("mlp.xml");
 }
 
-void Testa_MLP(Mat trainingDataMat, Mat labelsMat) {
+/* Funcao que testa a Multilayer Perceptron previamente treinada
+ * Parametros:
+ *              trainingDataMat - base de dados de teste
+ *              labelsMat       - saida esperada do teste
+ *              index           - indice da saida esperada
+ * Retorno:
+ *              index   - caso a taxa de acerto seja maior que 0.5
+ *              -1      - caso contrario
+ */
+int Testa_MLP(Mat trainingDataMat, Mat labelsMat, int index) {
+    // carrega os dados treinados e testa com a base inserida
     CvANN_MLP *mlp = new CvANN_MLP();
     mlp->load("mlp.xml");
     Mat resp(N_OUTPUT, N_OUTPUT, CV_32FC1);
     mlp->predict(trainingDataMat, resp);
     
-    int errados = 0, total = 0;
-    int predict = 0;
-    for (int i = 0; i < trainingDataMat.rows; i++) {
-        cout << " Predict: " << resp.at<float>(i) << " Erro: " << labelsMat.at<float>(i) - resp.at<float>(i) << endl;
-        
-        predict = (int)(resp.at<float>(i) + 0.5);
-        total++;
-        if(predict != (int)(labelsMat.at<float>(i)))  
-            errados++;
+    // para treinamento em tempo real, seleciona o indice com menor erro
+    if (index < 0) {
+        float min = 1.0;
+        for (int i = 0; i < N_OUTPUT; i++) {
+            float erro = labelsMat.at<float>(0,i) - resp.at<float>(0,i);
+            if(erro < min) {
+                min = erro;
+                index = i;
+            }
+        }
     }
-    cout << "Total: " << total << " Acuracia: " << 100.0 * ((float) (total - errados) / (float) total) << "%" << endl;
+    
+    // calcula a taxa de acerto
+    int errados = 0;
+    int predict = 0;
+    cout << "Predict: " << resp.at<float>(0,index) << " Erro: " << labelsMat.at<float>(0,index) - resp.at<float>(0,index) << endl;
+    predict = (int)(resp.at<float>(0,index) + 0.5);
+    if(predict != (int)(labelsMat.at<float>(0,index)))
+        errados++;
+    cout << "Erros: " << errados << endl;
+    if(errados)
+        return -1;
+    
+    return index;
+}
+
+/* Funcao que imprime qual movimento foi feito
+ * Parametros:
+ *              actualResult    - posicao da mao atual
+ *              lastResult      - posicao da mao anterior
+ * Referencia da posicao na tela:
+ * 0    1   2   3   4
+ * 5    6   7   8   9
+ * 10   11  12  13  14
+ */
+void CompareResult(int actualResult, int lastResult) {
+    if (lastResult < 0 || actualResult < 0)
+        return;
+    
+    String movimento;
+    
+    if ((lastResult - actualResult == 5) || (lastResult - actualResult == 10))
+        movimento = "cima";
+    else if ((actualResult - lastResult == 5) || (actualResult - lastResult == 10))
+        movimento = "baixo";
+    else if ((actualResult - lastResult < 5) && (actualResult - lastResult >= 0)) {
+        if ((actualResult%5) > (lastResult%5))
+            movimento = "direita";
+        else if ((actualResult%5) < (lastResult%5))
+            movimento = "diagonal esquerda para baixo";
+    }
+    else if ((lastResult - actualResult < 5) && (lastResult - actualResult >= 0)) {
+        if ((actualResult%5) < (lastResult%5))
+            movimento = "esquerda";
+        else if ((actualResult%5) > (lastResult%5))
+            movimento = "diagonal direita para cima";
+    }
+    else if ((actualResult - lastResult > 5) && (actualResult - lastResult <= 14)) {
+        if ((actualResult%5) < (lastResult%5))
+            movimento = "diagonal esquerda para baixo";
+        else if ((actualResult%5) > (lastResult%5))
+            movimento = "diagonal direita para baixo";
+    }
+    else if ((lastResult - actualResult > 5) && (lastResult - actualResult <= 14)) {
+        if ((actualResult%5) < (lastResult%5))
+            movimento = "diagonal esquerda para cima";
+        else if ((actualResult%5) > (lastResult%5))
+            movimento = "diagonal direita para cima";
+    }
+    
+    cout << movimento << endl;
 }
 
 int main(int argc, char **argv) {
     Mat image;
+    int flag = 0;
+    
+    cout << ("Escolha um opcao:" << endl << "[1] Treina a Rede" << endl "[2] Classifica uma imagem" << endl 
+        << "[3] Analisa sequencia de movimentos" << endl;
+    cin >> flag;
+    cin.ignore();
     
     if (flag == 1) {
         Mat labelsMat = Mat::zeros(N_OUTPUT, N_OUTPUT, CV_32FC1);
@@ -60,10 +143,10 @@ int main(int argc, char **argv) {
             image = imread(path_dir + image_file, CV_LOAD_IMAGE_GRAYSCALE);
             for (int i = 0; i < image.rows; i++) {
                 for (int j = 0; j < image.cols; j++) {
-                    trainingDataMat[k][i*image.cols+j] = image[i][j]/255.0;
+                    trainingDataMat.at<float>(k,i*image.cols+j) = image.at<float>(i,j)/255.0;
                 }
             }
-            labelsMat[k][k] = 1;
+            labelsMat.at<float>(k,k) = 1;
         }
         
         Treina_MLP(trainingDataMat, labelsMat);
@@ -84,18 +167,17 @@ int main(int argc, char **argv) {
         image = imread(path_dir + image_file, CV_LOAD_IMAGE_GRAYSCALE);
         for (int i = 0; i < image.rows; i++) {
             for (int j = 0; j < image.cols; j++) {
-                trainingDataMat[0][i*image.cols+j] = image[i][j]/255.0;
+                trainingDataMat.at<float>(0,i*image.cols+j) = image.at<float>(i,j)/255.0;
             }
         }
         
-        labelsMat[0][index] = 1;
-        Testa_MLP(trainingDataMat, labelsMat);
+        labelsMat.at<float>(0,index) = 1;
+        Testa_MLP(trainingDataMat, labelsMat,index);
     }
     else if (flag == 3) {
         Mat trainingDataMat = Mat::zeros(1, N_INPUT, CV_32FC1);
         Mat labelsMat = Mat::zeros(1,N_OUTPUT,CV_32FC1);
-        Mat lastResult = Mat::zeros(1,N_OUTPUT,CV_32FC1);
-        Mat actualResult = Mat::zeros(1,N_OUTPUT,CV_32FC1);
+        int lastResult = -1, actualResult = -1;
         VideoCapture cap(0);
         if(!cap.isOpened())
             return -1;
@@ -108,11 +190,11 @@ int main(int argc, char **argv) {
             Mat gray = cvtColor(frame, cv2.COLOR_BGR2GRAY)
             for (int i = 0; i < (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT); i++) {
                 for (int j = 0; j < (int) cap.get(CV_CAP_PROP_FRAME_WIDTH); j++) {
-                    trainingDataMat[0][(i*((int)cap.get(CV_CAP_PROP_FRAME_WIDTH)))+j] = frame[i][j]/255.0;   
+                    trainingDataMat.at<float>(0,(i*((int)cap.get(CV_CAP_PROP_FRAME_WIDTH)))+j) = frame.at<float>(i,j)/255.0;   
                 }
             }
             
-            actualResult = Testa_MLP(trainingDataMat,labelsMat);
+            actualResult = Testa_MLP(trainingDataMat,labelsMat,-1);
             CompareResult(actualResult,lastResult);
             lastResult = actualResult;
             
